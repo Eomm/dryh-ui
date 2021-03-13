9005aac5a579fa057fb5ac83edae6b2f068afc48
Manuel Spigolon <behemoth89@gmail.com>
2021-03-13 11:37:46 +0100

    add bootstrap

---
+++ src/MadCity.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import './MadCity.css';
+
+class MadCity extends React.Component {
+  render() {
+    return (
+      <Container className="p-3">
+        hello
+      </Container>
+    );
+  }
+}
+
+export { MadCity as App }
